e82f0299d5594105df6abe944afe22a02bb0afce
Tkd2007 <trankimdanh2007@gmail.com>
2026-08-11 14:07:17 +0700

    feat: new ocr_page function

--- src/ocr_baseline.py
+++ src/ocr_baseline.py
@@ -1,8 +1,11 @@
 import easyocr
 import numpy as np
+from PIL import Image
 
 reader = easyocr.Reader(['vi', 'en'])
 
 
 def ocr_page(image: Image.Image) -> str:
-    ...
+    image_array = np.array(image)
+    results = reader.readtext(image_array, detail=0)
+    return "\n".join(results)
